3c24d6a86c889802d253754c8bac80ee98302ce1
Diogo Filipe Diogo Filipe Toscano Lourinho Gomes <ddiogo-f@c1r1s10.42lisboa.com>
2025-01-16 12:58:15 +0000

    tests are more incremental

--- teste.c
+++ teste.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   teste.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: darkless12 <darkless12@student.42.fr>      +#+  +:+       +#+        */
+/*   By: ddiogo-f <ddiogo-f@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/14 14:10:32 by darkless12        #+#    #+#             */
-/*   Updated: 2025/01/15 18:33:13 by darkless12       ###   ########.fr       */
+/*   Updated: 2025/01/16 12:30:46 by ddiogo-f         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -53,7 +53,7 @@ int	main(void)
 	int		i;
 
 	vars.mlx = mlx_init();
-	vars.win = mlx_new_window(vars.mlx, 640, 480, "Que raio?");
+	vars.win = mlx_new_window(vars.mlx, 640, 480, "This is fine...");
 	img.img = mlx_new_image(vars.mlx, 640, 480);
 	img.addr = mlx_get_data_addr(img.img, &img.bits_per_pixel, &img.line_length,
 								&img.endian);
@@ -71,6 +71,111 @@ int	main(void)
 	mlx_loop(vars.mlx);
 }
 
+// #include <mlx.h>
+// #include <stdlib.h>
+
+// typedef struct	s_vars {
+// 	void	*mlx;
+// 	void	*win;
+// }				t_vars;
+
+// typedef struct	s_data {
+// 	void	*img;
+// 	char	*addr;
+// 	int		bits_per_pixel;
+// 	int		line_length;
+// 	int		endian;
+// }				t_data;
+
+// int	create_trgb(int t, int r, int g, int b)
+// {
+// 	return (t << 24 | r << 16 | g << 8 | b);
+// }
+
+// void	my_mlx_pixel_put(t_data *data, int x, int y, int color)
+// {
+// 	char	*dst;
+
+// 	dst = data->addr + (y * data->line_length + x * (data->bits_per_pixel / 8));
+// 	*(unsigned int*)dst = color;
+// }
+
+// int	main(void)
+// {
+// 	t_vars	vars;
+// 	t_data	img;
+// 	int		i;
+
+// 	vars.mlx = mlx_init();
+// 	vars.win = mlx_new_window(vars.mlx, 640, 480, "This is fine...");
+// 	img.img = mlx_new_image(vars.mlx, 640, 480);
+// 	img.addr = mlx_get_data_addr(img.img, &img.bits_per_pixel, &img.line_length,
+// 								&img.endian);
+// 	i = 0;
+// 	while (i < 200)
+// 	{
+// 		my_mlx_pixel_put(&img, 20, i + 20, create_trgb(0, 255, i, 0));
+// 		my_mlx_pixel_put(&img, 220, i + 20, create_trgb(0, 0, 255, i));
+// 		my_mlx_pixel_put(&img, i + 20, 20, create_trgb(0, i, 0, 255));
+// 		my_mlx_pixel_put(&img, i + 20, 220, create_trgb(0, i, 128, 128));
+// 		i++;
+// 	}
+// 	mlx_put_image_to_window(vars.mlx, vars.win, img.img, 0, 0);
+// 	mlx_loop(vars.mlx);
+// }
+
+// #include <mlx.h>
+// #include <stdlib.h>
+
+// typedef struct	s_vars {
+// 	void	*mlx;
+// 	void	*win;
+// }				t_vars;
+
+// typedef struct	s_data {
+// 	void	*img;
+// 	char	*addr;
+// 	int		bits_per_pixel;
+// 	int		line_length;
+// 	int		endian;
+// }				t_data;
+
+// int	create_trgb(int t, int r, int g, int b)
+// {
+// 	return (t << 24 | r << 16 | g << 8 | b);
+// }
+
+// void	my_mlx_pixel_put(t_data *data, int x, int y, int color)
+// {
+// 	char	*dst;
+
+// 	dst = data->addr + (y * data->line_length + x * (data->bits_per_pixel / 8));
+// 	*(unsigned int*)dst = color;
+// }
+
+// int	main(void)
+// {
+// 	t_vars	vars;
+// 	t_data	img;
+// 	int		i;
+
+// 	vars.mlx = mlx_init();
+// 	vars.win = mlx_new_window(vars.mlx, 640, 480, "This is fine...");
+// 	img.img = mlx_new_image(vars.mlx, 640, 480);
+// 	img.addr = mlx_get_data_addr(img.img, &img.bits_per_pixel, &img.line_length,
+// 								&img.endian);
+// 	i = 0;
+// 	while (i < 200)
+// 	{
+// 		my_mlx_pixel_put(&img, 20, i + 20, create_trgb(0, 255, 0, 0));
+// 		my_mlx_pixel_put(&img, 220, i + 20, create_trgb(0, 255, 0, 0));
+// 		my_mlx_pixel_put(&img, i + 20, 20, create_trgb(0, 255, 0, 0));
+// 		my_mlx_pixel_put(&img, i + 20, 220, create_trgb(0, 255, 0, 0));
+// 		i++;
+// 	}
+// 	mlx_put_image_to_window(vars.mlx, vars.win, img.img, 0, 0);
+// 	mlx_loop(vars.mlx);
+// }
 
 // #include <mlx.h>
 
