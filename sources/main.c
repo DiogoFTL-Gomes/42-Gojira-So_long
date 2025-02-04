bdb8780554c381711048937f727299d3a58f4d4e
Darkless12 <dftlg@yahoo.com>
2025-02-04 20:29:34 +0000

    update ligeiro. Investigar pq make não encontra mlx.h

--- sources/main.c
+++ sources/main.c
@@ -6,7 +6,7 @@
 /*   By: darkless12 <darkless12@student.42.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/23 15:49:25 by darkless12        #+#    #+#             */
-/*   Updated: 2025/02/04 20:20:17 by darkless12       ###   ########.fr       */
+/*   Updated: 2025/02/04 20:28:27 by darkless12       ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -47,3 +47,5 @@ int	main(int argc, char *argv[])
 	free(all);
 	return (0);
 }
+
+//https://harm-smits.github.io/42docs/libs/minilibx
